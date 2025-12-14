e80a1805f4203b9a0749879ea8cc0707ff92cff8
ValentinaPertile <valentinapertile@hotmail.com>
2025-12-14 16:23:41 -0300

    Mejora pàgina de contactos

--- FRONTEND/src/pages/Contacto.jsx
+++ FRONTEND/src/pages/Contacto.jsx
@@ -1,11 +1,13 @@
 const Contacto = () => (
 <div className="page-container">
     <h1>Contacto</h1>
-    <p>Puedes contactarnos a través de nuestros correos
+    <p>Puedes contactarnos a través de nuestros correos: 
             <li>juaniwilt@gmail.com</li>
-            <li>valentinapertile@gmail.com</li>
-            <li>franoyhenart@gmail.com</li>
+            <li>valentinapertile@hotmail.com</li>
+            <li>francoyhenart@gmail.com</li>
             <li>romeromacarena@gmail.com</li>
+            <li>lautaroflores@gmail.com</li>
+            
     </p>
 </div>
 );

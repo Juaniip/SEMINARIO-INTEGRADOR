2f0f52b15030c82c4eb250d5d9f8508a2ecfc63a
Juaniip <juaniwilt@gmail.com>
2025-12-15 13:54:17 -0300

    Preparado

--- FRONTEND/src/components/Footer.jsx
+++ FRONTEND/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© {new Date().getFullYear()} Mi Proyecto. Todos los derechos reservados.</p>
+      <p>© {new Date().getFullYear()} FORPW. Todos los derechos reservados.</p>
     </footer>
   );
 };
